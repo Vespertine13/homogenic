290a7875ae1f44ceb630c5c41eb65bccd96d0b28
Vespertine13 <eivind.kb@hotmail.com>
2023-04-26 16:44:51 +0200

    .

--- run.R
+++ run.R
@@ -1,8 +1,8 @@
 # run diagnostics
-file_filter <- "music"
+file_filter <- "images"
 source("main.R")
 plot_overview(plot_df)
 # run shell
-run_shells(df)
+# run_shells(df)
 # clear environment
 # rm(list= ls())
